a161bdec8ec6e3e645dcf291c7539a775278f872
Santiago <shinca12@eafit.edu.co>
2017-06-16 11:09:39 -0500

    FINAL!

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ library(RColorBrewer)
 library(scales)
 library(lattice)
 library(dplyr)
+library(ggplot2)
 library(plotly)
 
 # Leaflet bindings are a bit slow; for now we'll just sample to compensate
@@ -22,41 +23,49 @@ function(input, output, session) {
     # View(Year)
     countries <- readOGR("countries.geojson.txt", "OGRGeoJSON")
     countries$eafit <- rep(0, 177)
-    freq <- as.data.frame(table(data$País.de.residencia.[data$Año.de.grado <= input$year]))
+    freq <- as.data.frame(table(
+        data$País.de.residencia.[data$Año.de.grado <= input$year]))
     index <- match(c("Germany", "Argentina", "Australia",
                      "China", "Colombia", "Costa Rica",
                      "Denmark", "Spain", "United States",
                      "France", "Guatemala", "Hungary", "Italy",
-                     "Mexico", "Netherlands", "Peru","United Kingdom"), countries$name)
+                    "Mexico", "Netherlands", "Peru","United Kingdom"),
+                  countries$name)
     countries$eafit[index] <- countries$eafit[index] + freq$Freq[-1]
-    
-    state_popup <- paste0("<strong>Estado: </strong>", 
-                          countries$name, 
-                          "<br><strong>Numero de Egresados: </strong>", 
+
+    state_popup <- paste0("<strong>Estado: </strong>",
+                          countries$name,
+                          "<br><strong>Numero de Egresados: </strong>",
                           countries$eafit)
-    
+
     map <- leaflet(countries) %>% setView(lng = 10, lat = 20,  zoom = 2)
-    qpal <- function(y) unlist(Map( y == 0, f = function(x)(ifelse(x, "#F7FBFF", "#084594"))))
-    #print(qpal(countries$eafit))
+    qpal <- function(y) unlist(
+                 Map( y == 0, f = function(x)(ifelse(x, "#F7FBFF", "#084594"))))
     map %>%
       addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 1,
                 color = ~qpal(eafit), popup = state_popup)
   })
-  
+
   output$Gener <- renderPlotly({
-    plot_ly(as.data.frame(table(data$Genero[data$Año.de.grado <= input$year])), labels = ~Var1, values = ~Freq, type = "pie") 
+      plot_ly(as.data.frame(table(
+          data$Genero[data$Año.de.grado <= input$year])),
+              labels = ~Var1, values = ~Freq, type = "pie", hole= 0.6,
+              textposition = 'inside',
+                  textinfo = 'label+percent') %>%
+      layout(title = 'Distribucion por Genero')
   })
-  
+
   output$Carrera <- renderPlotly({
-    Carreras <- as.data.frame(table(data$Carrera[data$Año.de.grado <= input$year]))
+    Carreras <- as.data.frame(table(data$Carrera[data$Año.de.grado <= 2006]))
     df <- data.frame(Escuelas = c("Admin", "Ing", "Der", "Eco", "Sci"),
                      Freq = c(Reduce(`+`,Carreras[c(1,20,4,18),]$Freq),
                               Reduce(`+`,Carreras[c(9:13, 17),]$Freq),
                               Reduce(`+`,Carreras[5,]$Freq),
                               Reduce(`+`,Carreras[6:7,]$Freq),
                               Reduce(`+`,Carreras[c(14:16, 8),]$Freq)))
-    plot_ly(df, y=~Freq, x =~Escuelas, type = "bar", alpha = 0.6)
-    #plot_ly(as.data.frame(table(data$Carrera[data$Año.de.grado <= input$year])), x = ~Var1, y = ~Freq, type = 'histogram') 
+    plot_ly(df, y=~Freq, x =~Escuelas, type = "bar", alpha = 0.6) %>%
+        layout(title = 'Distribucion por escuelas',
+               yaxis = list(title = 'Frecuencia'))
   })
-  
+
 }
--- ui.R
+++ ui.R
@@ -1,9 +1,13 @@
 library(leaflet)
+library(ggplot2)
+library(plotly)
 
 navbarPage(img(src='logo_eafit_completo.png', width=70, height=35), id="nav",
   tabPanel("Centro de Egresados",
            h1("Centro de Egresados"),
-           tags$video(src = "Línea del Tiempo - Centro de Egresados - Universidad EAFIT.mp4", type = "video/mp4", autoplay = NA, controls = NA)),
+           tags$video(src =
+              "Línea del Tiempo - Centro de Egresados - Universidad EAFIT.mp4",
+              type = "video/mp4", autoplay = NA, controls = NA)),
   tabPanel("Mapa Interactivo",
     div(class="outer",
 
@@ -16,25 +20,20 @@ navbarPage(img(src='logo_eafit_completo.png', width=70, height=35), id="nav",
       leafletOutput("map", width="100%", height="100%"),
 
       # Shiny versions prior to 0.11 should use class="modal" instead.
-      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-        width = 330, height = "auto",
+      absolutePanel(id = "controls", class = "panel panel-default",
+                    fixed = TRUE, draggable = TRUE, top = 60, left = "auto",
+                    right = 20, bottom = "auto", width = 330, height = "auto",
 
-        h2("Localizacion de los eafitences"),
-        sliderInput("year", "Año", min = 2001, max = 2017, value = 2001, animate = animationOptions(interval = 5000, loop = F)),
-        plotlyOutput("Gener", height = 200),
-        plotlyOutput("Carrera", height = 200)
+        h2("Localizacion de los eafitenses"),
+        sliderInput("year", "Año", min = 2001, max = 2017, value = 2001,
+                    animate = animationOptions(interval = 5000, loop = F)),
+        plotlyOutput("Gener", height = 250, width = 250),
+        plotlyOutput("Carrera", height = 250, width = 250)
       ),
 
       tags$div(id="cite",
         'Vigilada Mineducación.'
       )
     )
-  )#,
-
-  #tabPanel("Titulo",
-  #         titlePanel("Titulo"),
-  #         sliderInput("año", "Año", min = 2001, max = 2017, value = 2001, animate = animationOptions(interval = 5000, loop = F))
-
-  #)
+  )
 )
